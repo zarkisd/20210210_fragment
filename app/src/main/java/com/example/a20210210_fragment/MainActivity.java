833b44fe8bfb885336e2b5ea5fb6adfe4a45f034
junho <xxjunhoxx@naver.com>
2021-02-10 14:21:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20210210_fragment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20210210_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
